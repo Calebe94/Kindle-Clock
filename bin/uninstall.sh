bf2b20c5343ac550df2002bd0d84e091c1e9bdc3
Calebe94 <calebe94@disroot.org>
2023-01-04 19:06:46 -0300

    Renamed project to "Clock"
    * Added clock.html page;
    * Added clock.html as default page;

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ###############################################################################
-## Configuration variables 
+## Configuration variables
 
-APP_FOLDER="/mnt/us/extensions/WebLaunch"
+APP_FOLDER="/mnt/us/extensions/Clock"
 
 APPREG="/var/local/appreg.db"
 
-APP_ID="com.PaulFreund.WebLaunch"
+APP_ID="com.Calebe94.Clock"
 APP_ID_STRING="'$APP_ID'"
 
 ## WARNING - Changing the extension path after first run will result in errors,
@@ -19,7 +19,7 @@ sqlite3 $APPREG "delete from handlerIds where handlerId=$APP_ID_STRING"
 sqlite3 $APPREG "delete from properties where handlerId=$APP_ID_STRING"
 
 ###############################################################################
-## Uninstall powerbutton filter 
+## Uninstall powerbutton filter
 
 FILE_RULES="/etc/udev/rules.d/98-yoshibutton.rules"
 FILE_NOTIFY="/lib/udev/bin/notifyyoshibutton"
